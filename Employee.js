cd11cd99c7aad53d9fa5f06b7b082310ae001f88
unknown <Greg.Marshall.Hill@gmail.com>
2017-09-01 15:56:37 -0500

    Complete DiskIO JSON project and added JSDoc3 comments

--- Employee.js
+++ Employee.js
@@ -2,15 +2,45 @@
 
 const fs = require('fs')
 
+/**
+ * @name Employee
+ * @author Greg Hill
+ * @classdesc This class has three properties, name, title, and salary. It can be promoted or unmarshalled from a .json file
+ */
 class Employee {
-  constructor (/* ??? */) {
-    // TODO
+  
+  /**
+   * @constructs Employee
+   * @description The constructor for the Employee class
+   * @param employeeData An object containing name, title, and salary key value pairs
+   */
+  constructor (employeeData) {
+    this.name = employeeData.name
+    this.title = employeeData.title
+    this.salary = employeeData.salary
   }
-
-  // TODO ???
+  
+  /**
+   * @function parseFromFilePath
+   * @description A static function that unmarshalls an employee object from a .json file
+   * @param pathName The path where the json file is located
+   * @returns A new employee object unmarshalled from the .json file
+   */
+  static parseFromFilePath(pathName) {
+    return new Employee(JSON.parse(fs.readFileSync(pathName)))
+  }
+  
+  /**
+   * @function promote
+   * @description Changes the title and salary of this Employee
+   * @param newTitle The new title for this Employee
+   * @param newSalary The new salary for this Employee
+   */
+  promote(newTitle, newSalary) {
+    this.title = newTitle
+    this.salary = newSalary
+  }
 }
-
-// TODO ???
 
 module.exports = {
   Employee
--- main.js
+++ main.js
@@ -4,11 +4,7 @@ const util = require('util')
 const path = require('path')
 const { Employee } = require('./Employee')
 
-const employee =
-  Employee
-    .parseFromFilePath(
-      path.resolve(__dirname, 'employee.json')
-    )
+const employee = Employee.parseFromFilePath(path.resolve(__dirname, 'employee.json'))
 
 console.log(`is Employee? ${employee instanceof Employee}`)
 console.log(`parsed: ${util.inspect(employee)}`)
